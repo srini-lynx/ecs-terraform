2dedb309ccde5cdcb874600b5f1344f44d2fe6f5
vagrant <vagrant@dev-node038>
2023-06-10 17:55:59 +0000

    main.tf added

---
+++ main.tf
@@ -0,0 +1,65 @@
+# Create an ECS cluster
+resource "aws_ecs_cluster" "my_cluster" {
+  name = "srini-ecs-cluster"
+}
+
+# Create a task definition
+resource "aws_ecs_task_definition" "my_task_definition" {
+  family                   = "my-task-family-test"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = 1024
+  memory                   = 2048
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+  container_definitions    = <<EOF
+[
+  {
+    "name": "my-container",
+    "image": "srini78/nodejswebapp:latest",
+    "portMappings": [
+      {
+        "containerPort": 80,
+        "hostPort": 80
+      }
+    ]
+  }
+]
+EOF
+}
+
+# Create a service to run the task on the cluster
+resource "aws_ecs_service" "my_service" {
+  name            = "my-service"
+  cluster         = aws_ecs_cluster.my_cluster.id
+  task_definition = aws_ecs_task_definition.my_task_definition.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+  network_configuration {
+    subnets          = ["subnet-051b423ebf29bf8db"]
+    security_groups  = ["sg-07c06cdac34f7b428"]
+    assign_public_ip = true
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.ecs_task_execution_role.name
+}
